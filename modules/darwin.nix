830af325a0f8e7f65d5c432373ef28aa55981e20
Luke Barton <luke@lukebarton.co.uk>
2026-02-14 14:34:32 +0000

    feat: allow apple watch authentication

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -20,6 +20,7 @@
   security.pam.services.sudo_local = {
     enable = true;
     touchIdAuth = true;
+    watchIdAuth = true; # use apple watch for auth, if available
   };
 
   # System preferences
