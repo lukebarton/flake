cce1fd2c7bcf73d0f194b776b77d51cda458912f
Luke Barton <luke@lukebarton.co.uk>
2026-02-14 23:37:44 +0000

    feat: significant aerospace config rework, proper workspaces, decent modes

--- configs/aerospace.nix
+++ configs/aerospace.nix
@@ -1,6 +1,7 @@
-{ ... }: {
+{ config, ... }: {
   # Config
-  home.file.".config/aerospace/aerospace.toml".source = ../files/aerospace/aerospace.toml;
+  #  home.file.".config/aerospace/aerospace.toml".source = ../files/aerospace/aerospace.toml;
+  home.file.".config/aerospace/aerospace.toml".source = config.lib.file.mkOutOfStoreSymlink "/Users/luke/src/github.com/lukebarton/flake/files/aerospace/aerospace.toml";
 
   # Sticky windows script
   home.file.".config/aerospace/sticky-windows.sh".source = ../files/aerospace/sticky-windows.sh;
