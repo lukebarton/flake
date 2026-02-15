12979ae0d1d7e9e54d411dd59a0e246167a03330
Luke Barton <luke@lukebarton.co.uk>
2026-02-15 15:50:33 +0000

    feat: add aerospace defaults

--- configs/aerospace.nix
+++ configs/aerospace.nix
@@ -3,6 +3,10 @@
   #  home.file.".config/aerospace/aerospace.toml".source = ../files/aerospace/aerospace.toml;
   home.file.".config/aerospace/aerospace.toml".source = config.lib.file.mkOutOfStoreSymlink "/Users/luke/src/github.com/lukebarton/flake/files/aerospace/aerospace.toml";
 
+  targets.darwin.defaults."bobko.aerospace" = {
+    displayStyle = "squares";
+  };
+
   # Sticky windows script
   home.file.".config/aerospace/sticky-windows.sh".source = ../files/aerospace/sticky-windows.sh;
 }
