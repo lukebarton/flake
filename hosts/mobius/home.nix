5aea0a2720791c91be63333febab61a7fccb8844
Luke Barton <luke@lukebarton.co.uk>
2026-02-21 23:58:15 +0000

    refactor: compat with programs.ssh breaking changes

--- hosts/mobius/home.nix
+++ hosts/mobius/home.nix
@@ -8,10 +8,12 @@
 
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host *
-        IdentityAgent ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-    '';
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      extraOptions = {
+        IdentityAgent = "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      };
+    };
   };
 
   home.file.".config/rclone/cloud.filters".text = ''
