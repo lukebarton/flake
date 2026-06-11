304ca3cc575e19387d6ebe99524f09c8e1eb4b54
Luke Barton <luke@lukebarton.co.uk>
2026-06-11 14:44:43 +0100

    fix: remove pi-coding-agent from pnpm global packages
    The package is no longer needed, so drop it from the activation
    script that installs global pnpm packages.

--- modules/home/pnpm.nix
+++ modules/home/pnpm.nix
@@ -4,7 +4,6 @@
   home.activation.pnpmGlobalPackages = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     export PNPM_HOME="$HOME/.local/share/pnpm"
     export PATH="$PATH:$PNPM_HOME/bin"
-    ${pkgs.pnpm}/bin/pnpm add -g @mariozechner/pi-coding-agent
   '';
 
   programs.zsh.sessionVariables = {
